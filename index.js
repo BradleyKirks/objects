dda4a1ee9fc092c00c793bb38494449e467c5383
Bradley Kirks <spinnyorb23@aim.com>
2023-10-05 16:55:52 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const person = {
+    firstname: "Todd",
+    age: 28
+
+}
+    console.log(person.firstname) 
+    // add one to the persons age 
+    person.age++
+    
+    person.lastname = "Albert" // add a new property to the object
+    
+    console.log(person.age) 
+    console.log(person.lastname) 
